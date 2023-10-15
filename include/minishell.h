c03760f08dfc1c9c28a2d240d80c4f6adeeb05e2
KaiMiyazawa <124577165+KaiMiyazawa@users.noreply.github.com>
2023-10-15 12:44:45 +0900

    Add files via upload

---
+++ include/minishell.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miyazawa.kai.0823 <miyazawa.kai.0823@st    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/15 12:33:21 by miyazawa.ka       #+#    #+#             */
+/*   Updated: 2023/10/15 12:43:57 by miyazawa.ka      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+# include <signal.h>
+# include <unistd.h>
+
+#endif
