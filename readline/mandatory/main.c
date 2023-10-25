aaab7703a758020fa10cce37b7e33344a0ade642
KaiMiyazawa <124577165+KaiMiyazawa@users.noreply.github.com>
2023-10-25 11:08:39 +0900

    ファイル分割 と 履歴の変更 と readline変更

---
+++ readline/mandatory/main.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miyazawa.kai.0823 <miyazawa.kai.0823@st    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 12:08:46 by kmiyazaw          #+#    #+#             */
+/*   Updated: 2023/10/24 14:28:10 by miyazawa.ka      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+//   FUNCTIONS
+
+// readline
+// rl_clear_history
+// rl_on_new_line
+// rl_replace_line
+// rl_redisplay
+// add_history
+// printf
+// malloc
+// free
+// write
+// access
+// open
+// read
+// close
+// fork
+// wait
+// waitpid
+// wait3
+// wait4
+// signal
+// sigaction
+// sigemptyset
+// sigaddset
+// kill
+// exit
+// getcwd
+// chdir
+// stat
+// lstat
+// fstat
+// unlink
+// execve
+// dup
+// dup2
+// pipe
+// opendir
+// readdir
+// closedir
+// strerror
+// perror
+// isatty
+// ttyname
+// ttyslot
+// ioctl
+// getenv
+// tcsetattr
+// tcgetattr
+// tgetent
+// tgetflag
+// tgetnum
+// tgetstr
+// tgoto
+// tputs
+
+void	data_init(int ac, char **av, char **ev, t_data *data)
+{
+	g_state = 0;
+	data->ac = ac;
+	data->av = av;
+	data->envp = ev;
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_data	data;
+
+	data_init(argc, argv, envp, &data);
+	minishell(&data);
+	exit (0);
+}
+
+//__attribute__((destructor))
+//static void destructor() {
+//	system("leaks -q a.out");
+//}
---
+++ readline/mandatory/minishell.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miyazawa.kai.0823 <miyazawa.kai.0823@st    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/15 13:31:59 by miyazawa.ka       #+#    #+#             */
+/*   Updated: 2023/10/25 11:06:26 by miyazawa.ka      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+void	handler(int sig)
+{
+	if (sig == SIGINT)
+	{
+		ioctl(STDIN_FILENO, TIOCSTI, "\n");
+		rl_replace_line("", 0);
+		rl_on_new_line();
+	}
+}
+
+void	handle_signals(void)
+{
+	struct sigaction	sa;
+
+	sa.sa_handler = handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	if (sigaction(SIGINT, &sa, NULL) == -1)
+		exit (1);
+	if (signal(SIGQUIT, SIG_IGN) == SIG_ERR)
+		exit (1);
+}
+
+char	*readline_e(char *prompt)
+{
+	char	*line;
+
+	line = NULL;
+	line = readline(prompt);
+	if (line == NULL)
+	{
+		printf(" EOF\nexit\n");
+		exit(1);
+	}
+	return (line);
+}
+
+void	minishell(t_data *data)
+{
+	char				*line;
+
+	//killコマンドでctrl+Cとかの挙動確認する用
+	pid_t				pid;
+	pid = getpid();
+	printf("%d\n", pid);//プロセス番号の確認
+
+	line = NULL;
+	handle_signals();
+	while (1)
+	{
+		line = readline_e("> ");
+		if (ft_strlen(line) > 0)
+			add_history(line);
+		if (!strcmp(line, "exit"))
+		{
+			free(line);
+			break ;
+		}
+		else if (strlen(line) != 0)
+		{
+			lexer(line, data);
+			perser(data);
+			evaluater(data);
+		}
+		free(line);
+		line = NULL;
+	}
+	printf("exit\n");
+}
