adf56954d303616682a98a164054b3c21c57cc97
KaiMiyazawa <124577165+KaiMiyazawa@users.noreply.github.com>
2023-10-29 18:43:58 +0900

    Update main.c

--- mandatory/main.c
+++ mandatory/main.c
@@ -12,45 +12,6 @@
 
 #include"../include/minishell.h"
 
-void	add_str(char *s1, char *s2)
-{
-	int	count;
-	int	count2;
-
-	count = 0;
-	count2 = 0;
-	while (s1[count] != '\0')
-		count++;
-	while (s2[count2] != '\0')
-		s1[count++] = s2[count2++];
-	s1[count] = '\0';
-}
-
-void	print_error(char *error_msg, char *addition, bool exit_state, t_data *d)
-{
-	char	*error;
-	int		size;
-
-	size = 11 + ft_strlen(error_msg) + 1 + 1;
-	if (addition != NULL)
-		size += 2 + ft_strlen(addition);
-	error = (char *)ft_calloc(sizeof(char), size);
-	if (!error)
-		perror(strerror(errno));
-	ft_strlcpy(error, "minishell: ", 12);
-	if (addition != NULL)
-	{
-		add_str(error, addition);
-		add_str(error, ": ");
-	}
-	add_str(error, error_msg);
-	add_str(error, "\n");
-	if (write(2, error, size) == FAILED)
-		perror(strerror(errno));
-	free(error);
-	g_state = exit_state;
-}
-
 void	data_init(int ac, char **av, char **ev, t_data *data)
 {
 	g_state = 0;
